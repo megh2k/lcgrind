4fa11a7861ee7050093961d0cba74ad8024a7b49
Megh <hirparamegh@gmail.com>
2025-01-07 18:44:44 -0500

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-// import Loading from "./loading";
-// import { Suspense } from "react";
 
 const raleway = Raleway({ subsets: ["latin"], weight: "400" });
 
@@ -19,7 +17,8 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={{ backgroundColor: "#FFFDF0" }}
+      <body
+        style={{ backgroundColor: "#FFFDF0" }}
         className={`${raleway.className} min-h-screen text-gray-800`}
       >
         <script
@@ -28,12 +27,10 @@ export default async function RootLayout({
           src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/helix.js"
         ></script>
         <main className="min-h-screen flex flex-col">
-          {/* <Suspense fallback={<Loading />}> */}
-            <Navbar />
-            <div className="mx-auto px-4 sm:px-6 lg:px-8 w-full max-w-screen-xl">
-              <div>{children}</div>
-            </div>
-          {/* </Suspense> */}
+          <Navbar />
+          <div className="mx-auto px-4 sm:px-6 lg:px-8 w-full max-w-screen-xl">
+            <div>{children}</div>
+          </div>
         </main>
       </body>
     </html>
