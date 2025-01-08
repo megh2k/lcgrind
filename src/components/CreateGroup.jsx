3e2ad8d4e4012365b829adaef12599363819d5aa
Megh <hirparamegh@gmail.com>
2025-01-08 12:48:59 -0500

    url

--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -16,7 +16,7 @@ export default function CreateGroup({ user }) {
     else {
       try {
         const response = await fetch(
-          `${process.env.APP_URL}/api/groups/create/`,
+          `${process.env.NEXTAUTH_URL}/api/groups/create/`,
           {
             method: "POST",
             headers: {
